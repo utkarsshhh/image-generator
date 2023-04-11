1124a8d86e7ab17e64c075274699627fc0e1d2e5
Utkarsh Padia <utkarshpadia@gmail.com>
2023-04-12 00:33:41 +0530

    chore: add requirements

--- main.py
+++ main.py
@@ -1,35 +1,9 @@
+# Importing the libraries
 import torch
-from torchvision import datasets,transforms
-
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
-trainset = datasets.MNIST('MNIST_data/',download=True,train=True,transform= transform)
-trainloader = torch.utils.data.DataLoader(trainset,batch_size = 64,shuffle = True)
-
-def sigmoid_function(x):
-    '''
-
-    This function transforms any input parameter to the range of (0,1) according
-    to the sigmoid function f(x) = 1/(1+exp(-x))
-
-    Input:
-    x - a tensor of random numerical values
-
-    Output:
-    returns the tensor with the values transformed in between the range (0,1)
-    '''
-
-    return (1/(1+torch.exp(-x)))
-
-torch.manual_seed(7)
-
-features = torch.randn((1,5))
-weights = torch.randn_like(features)
-bias = torch.randn((1,1))
-
-neural_output = torch.add(torch.mm(features,torch.transpose(weights,0,1)),bias)
-
-y = sigmoid_function(neural_output)
-
-print (y)
+from torch import nn
+from torchvision import transforms
+from torchvision.datasets import MNIST # Training dataset
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
