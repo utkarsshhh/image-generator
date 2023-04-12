baad205a446ccc43df16b2febac36c78584ee27a
Utkarsh Padia <utkarshpadia@gmail.com>
2023-04-13 01:07:53 +0530

    chore: function for defining a layer of the generator

--- main.py
+++ main.py
@@ -7,3 +7,24 @@ from torchvision.utils import make_grid
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
+## Defining the layer of the Generator
+
+def generator_layer(input_dim,output_dim):
+    '''
+
+    This function creates a layer of the generator neural network.
+
+    Input:
+    input_dim: It is the dimension of the layer's input, a scaler
+    output_dim: It is the dimension of the layer's output vector
+
+    Output:
+    returns the output of the layer after linear transformation followed by batch normalisation
+    and ReLU activation function
+
+    '''
+    return nn.Sequential(
+        nn.Linear(input_dim,output_dim),
+        nn.BatchNorm1d(output_dim),
+        nn.ReLU(inplace = True)
+    )
